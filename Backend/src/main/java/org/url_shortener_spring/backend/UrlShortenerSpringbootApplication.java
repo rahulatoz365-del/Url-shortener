e0ef0321c8e753df0fa1bdffa2a39fa62e924704
Rahul Singh <146085626+rahulatoz365-del@users.noreply.github.com>
2026-02-01 10:57:10 +0530

    Enable asynchronous processing in application

--- Backend/src/main/java/org/url_shortener_spring/backend/UrlShortenerSpringbootApplication.java
+++ Backend/src/main/java/org/url_shortener_spring/backend/UrlShortenerSpringbootApplication.java
@@ -1,5 +1,5 @@
 package org.url_shortener_spring.backend;
-
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
